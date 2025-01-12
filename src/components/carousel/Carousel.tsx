6c886e5792b434119e4899645799660a8cc72e07
matheus <matheus.felipe.19rt@gmail.com>
2025-01-12 20:35:38 -0300

    feat(ui): create and implement Carousel component in Home page

---
+++ src/components/carousel/Carousel.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import { previewPresets } from "@/src/data/previewPresets"
+import { Icon } from "@iconify/react"
+import { useState } from "react"
+import CarouselPreview from "./CarouselPreview"
+
+export const Carousel = () => {
+	const [currentIndex, setCurrentIndex] = useState(0)
+
+	const nextCard = () => {
+		setCurrentIndex((prevIndex) => (prevIndex + 1) % previewPresets.length)
+	}
+
+	const prevCard = () => {
+		setCurrentIndex((prevIndex) => (prevIndex - 1 + previewPresets.length) % previewPresets.length)
+	}
+
+	return (
+		<div className="relative flex h-[500px] items-center justify-center">
+			<button
+				onClick={prevCard}
+				className="absolute -left-9 z-10 flex items-center justify-center rounded-full"
+				style={{ top: "50%", transform: "translateY(-50%)" }}
+			>
+				<Icon icon="ri:arrow-left-s-line" className="icon size-8 hover:text-muted" />
+			</button>
+
+			<div className="flex items-center">
+				<CarouselPreview presetId={currentIndex} />
+			</div>
+
+			<button
+				onClick={nextCard}
+				className="absolute -right-9 z-10 flex items-center justify-center rounded-full"
+				style={{ top: "50%", transform: "translateY(-50%)" }}
+			>
+				<Icon icon="ri:arrow-right-s-line" className="icon size-8 hover:text-muted" />
+			</button>
+		</div>
+	)
+}
